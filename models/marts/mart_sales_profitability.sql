a0c4b409028d33c58b1252a42bfbfeac1c105015
Mohamed Rafi <mohamedrafi.emirates@gmail.com>
2026-07-05 22:55:49 +0400

    Align profitability mart columns with Lightdash schema

--- models/marts/mart_sales_profitability.sql
+++ models/marts/mart_sales_profitability.sql
@@ -14,14 +14,14 @@ select
     fs.quantity,
     fs.unit_price,
     fs.unit_cost,
-    fs.sales_amount,
-    fs.cost_amount,
+    fs.sales_amount as total_sales,
+    fs.cost_amount as total_cost,
     fs.gross_profit,
-    safe_divide(fs.gross_profit, fs.sales_amount) as gross_margin_pct
+    fs.gross_profit as net_profit,
+    safe_divide(fs.gross_profit, fs.sales_amount) as gross_margin_pct,
+    safe_divide(fs.gross_profit, fs.sales_amount) as net_margin_pct
 from {{ ref('fact_sales') }} fs
 left join {{ ref('dim_customers') }} dc
     on fs.customer_id = dc.customer_id
 left join {{ ref('dim_products') }} dp
-    on fs.product_id = dp.product_id
-
-    
+    on fs.product_id = dp.product_id    
